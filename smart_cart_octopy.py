4ddf506e5825f6102d35d3d246a44dfff094d4d7
Jesus Vazquez <jmvazqueznicolas@gmail.com>
2021-07-21 16:03:20 -0500

    OCR intergrado

--- smart_cart_octopy.py
+++ smart_cart_octopy.py
@@ -6,38 +6,28 @@ import os
 from config import files
 from config import paths
 import time
+import re
+
 # Este biblioteca requiere de:  
 # sudo apt-get update -y
 # sudo apt-get install -y tesseract-ocr     
 import pytesseract
 from pytesseract import Output
 
-"""
-import easyocr
-
-gpus = tf.config.list_physical_devices('GPU')
-if gpus:
-    try:
-        tf.config.experimental.set_virtual_device_configuration(
-            gpus[0], [tf.config.experimental.VirtualDeviceConfiguration(memory_limit=520)])
-    except RuntimeError as e:
-        print(e)
-"""
-
-# Esta linea es para instalar pzbar en el s.o. ya instalada se puede comentar
+# Estas linea son para instalar pzbar en el s.o. ya instalada se puede comentar
 """
 os.system("sudo apt-get install libzbar0")
 os.system("sudo apt install gcc")
 os.system("pip install pyzbar")
 """
 
-os.system("v4l2-ctl -d /dev/video0 --list-ctrls")
-os.system("v4l2-ctl -d /dev/video0 --set-ctrl=focus_auto=1")
-#os.system("v4l2-ctl -d /dev/video0 --set-ctrl=focus_absolute=70")
-os.system("v4l2-ctl -d /dev/video0 --list-ctrls")
-
-
 from pyzbar import pyzbar
+
+# Estas lineas son para configurar la cámara Logitech (Quitar autofocus)
+os.system("v4l2-ctl -d /dev/video0 --list-ctrls")
+os.system("v4l2-ctl -d /dev/video0 --set-ctrl=focus_auto=0")
+os.system("v4l2-ctl -d /dev/video0 --set-ctrl=focus_absolute=70")
+os.system("v4l2-ctl -d /dev/video0 --list-ctrls")
 
 # Estas lineas permiten instalar TensorFlow Object Detection API, una vez instalado se pueden comentar
 """
@@ -58,13 +48,12 @@ from object_detection.builders import model_builder
 from object_detection.utils import config_util
 
 # Para llevar a cabo la ejecución se necesitan algunos archivos
-# 1.- Los archivos ckpt-3
+# 1.- Los archivos ckpt-*
 # 2.- pipeline.config
 # Estos deben de estar en la carpet /Tensorflow/workspace/models/[MODELO_UTILIZADO]/
 # Otro archivo necesario es
 # 1.- label_map.pbtxt
 # El cual debe estar en la carpeta Tensorflow/workspace/annotations/
-
 
 # Se cargan los archivos para generar el modelo de detección 
 configs = config_util.get_configs_from_pipeline_file(files['PIPELINE_CONFIG'])
@@ -82,7 +71,7 @@ def detect_fn(image):
 
 # Se utiliza el for para buscar la primer cámara disponible, en un sistema de multiples cámaras
 for i in range(20):
-    cap = cv2.VideoCapture(0)
+    cap = cv2.VideoCapture(i)
     cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1280) # 1920
     cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 720) # 1080 
     width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
@@ -102,23 +91,31 @@ model = cv2.ml.KNearest_create()
 model.train(samples, cv2.ml.ROW_SAMPLE, responses)
 
 # Configuración para OCR
-custom_config = r'--oem 3 --psm 8 -c tessedit_char_whitelist=0123456789'
-
-# Erosion
-def erode(image):
-    kernel = np.ones((5,5),np.uint8)
-    return cv2.erode(image, kernel, iterations = 1)
-
-# Escala de grises
-def get_grayscale(image):
-    return cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-
-# Binarización
-def thresholding(image):
-    return cv2.threshold(image, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)[1]
+custom_config = r'--oem 3 --psm 6 -c tessedit_char_whitelist=0123456789'
 
 cont_detec = 0
 t0 = time.time()
+
+# Diccionario de productos
+productos = {'7501088210709':'Cremino', '7501791600682':'Cafe soluble', '725226003504':'Pulparindo', '0025046021499':'Duvalin',
+            '7501025405694':'Toallitas', '7501954906644':'Cafe de grano'}
+carrito = []
+
+producto_detec = {'nombre':'Sin producto', 'codigo':''}
+
+
+# Funciones básicas de pre-procesamiento
+# Escala de grises
+def get_grayscale(image):
+    return cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+# Binarización
+def thresholding(image):
+    return cv2.threshold(image, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)[1]
+# Dilatacion
+def dilate(image):
+    kernel = np.ones((2,2),np.uint8)
+    return cv2.dilate(image, kernel, iterations = 1)
+
 # Lectura continua de la cámara
 while cap.isOpened(): 
     ret, frame = cap.read()
@@ -127,7 +124,6 @@ while cap.isOpened():
     
     # Detección de los códigos de barras
     prediction_dict, detections = detect_fn(input_tensor)
-
     num_detections = int(detections.pop('num_detections'))
     detections = {key: value[0, :num_detections].numpy()
                   for key, value in detections.items()}
@@ -145,15 +141,18 @@ while cap.isOpened():
     min_score_tresh = 0.8
     tboxes = boxes[scores > min_score_tresh]
     
+    # Para cada una de las ROIs con códigos
     coor_boxes = []
     for box in tboxes:
         ymin, xmin, ymax, xmax = box
         ancho_img = ymax - ymin
         largo_img = xmax - xmin
         box_loc = [xmin*width, xmax*width, ymin*height, ymax*height]
+        # Padding para intentar capturar el número del código de barras
         ancho_img = box_loc[3] - box_loc[2]
         largo_img = box_loc[1] - box_loc[0]
         box_loc = [box_loc[0]-largo_img*0.03, box_loc[1]+largo_img*0.03, ymin*height-ancho_img*0.05, ymax*height+ancho_img*0.05]
+        # Si el padding sale de la imagen, se lo quita
         for valor in box_loc:
             if valor <= 0:
                 box_loc = [xmin*width, xmax*width, ymin*height, ymax*height]
@@ -163,69 +162,55 @@ while cap.isOpened():
     # Permite recortar el código de barras de la imagen
     for coor in coor_boxes:
         crop_image = frame[coor[2]:coor[3], coor[0]:coor[1]]
-        crop_for_ocr = frame[coor[2]+int(ancho_img*0.80):coor[3], coor[0]:coor[1]]
-        #cv2.imshow('Codigo detectado', crop_for_ocr)
-
+        
+        
         # Procesamiento para el OCR
         ocr_gris = get_grayscale(crop_image)
         ocr_bin = thresholding(ocr_gris)
-
         output = cv2.connectedComponentsWithStats(ocr_bin, 8, cv2.CV_32S)
         (numLabels, labels, stats, centroids) = output
         mask = np.zeros(ocr_gris.shape, dtype="uint8")
-
+        # Se eliminan las regiones largas y delgadas (lineas del código de barras)
         for i in range(1, numLabels):
             area = stats[i, cv2.CC_STAT_AREA]
             x = stats[i, cv2.CC_STAT_LEFT]
             y = stats[i, cv2.CC_STAT_TOP]
             w = stats[i, cv2.CC_STAT_WIDTH]
             h = stats[i, cv2.CC_STAT_HEIGHT]
-            #print("Elemento", i, "tiene un area de", area)
-            #print("Elemento", i, "tiene un largo de ", w)
             keepHeight = h < 50 #and h < 65
             keepWidth = w < 50 #and w < 50
-
             if all((keepHeight, keepWidth)):
                 #print("[INFO] keeping connected component '{}'".format(i))
                 componentMask = (labels == i).astype("uint8") * 255
                 mask = cv2.bitwise_or(mask, componentMask)
-            
-        cv2.imshow("Image", crop_image)
-        print("Tamaño de la mascara", mask.shape)
-        print("Tamaño del recorte", crop_image.shape)
-        print("Las coordenadas", coor)
-        #crop_for_ocr = mask[coor[2]+int(ancho_img*0.80):coor[3], coor[0]:coor[1]]
-        #cv2.imshow("Characters", crop_for_ocr)
-        #codigo_ocr = pytesseract.image_to_string(crop_for_ocr, config=custom_config)
-        #print('El código es: ',codigo_ocr)
 
-        """
-        if len(codigo_ocr) >= 15:
-            print(codigo_ocr.split(), len(codigo_ocr))
-            if len(codigo_ocr) > 17:
-                print(codigo_ocr[1:13])
-            else:
-                print(codigo_ocr[0:13])
-        """
-        # Guardar imagen
-        # Filename
-        filename = 'codigo'+str(cont_detec)+'.jpg'
-        #cv2.imwrite(filename, crop_image)
-
-
-        #Codigo de barras
-        barcodes = pyzbar.decode(crop_image)
-        for barcode in barcodes:
-            code_value = barcode.data.decode("utf-8")
-            #print(f"El código detectado es {code_value}")
-            cont_detec += 1
-            #print(f"El número codigos detectados es: {cont_detec}")
+        crop_for_ocr = mask[int(0.7*mask.shape[0]):mask.shape[0], 0:mask.shape[1]]
+        crop_for_ocr = dilate(crop_for_ocr)
+        crop_for_ocr = cv2.threshold(crop_for_ocr, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)[1]
+        codigo_ocr = pytesseract.image_to_string(crop_for_ocr, config=custom_config)
+        codigo_ocr = re.sub('\D', '', codigo_ocr)
         
-        #OCR
-        #lect = easyocr.Reader(['en'])
-        #numero_barras = lect.readtext(crop_for_ocr)
-        #print(numero_barras)
+        #Codigo de barras
+        barcode = pyzbar.decode(crop_image)
+        #for barcode in barcodes:
+        if barcode:
+            code_value = barcode[0].data.decode("utf-8")
+        else:
+            code_value = ''
 
+        # Producto nuevo?
+        if (producto_detec['codigo'] != (code_value or codigo_ocr)) and ((code_value or codigo_ocr) in productos):
+            # Se actualiza producto
+            try:
+                producto_detec = {'nombre':productos[code_value], 'codigo':code_value}
+            except:
+                producto_detec = {'nombre':productos[codigo_ocr], 'codigo':codigo_ocr}
+            carrito.append(producto_detec['nombre'])
+            print('El contenido del carrito es: ')
+            for producto in carrito:
+                print(producto)
+            
+            
     viz_utils.visualize_boxes_and_labels_on_image_array(
                 image_np_with_detections,
                 detections['detection_boxes'],
@@ -237,13 +222,28 @@ while cap.isOpened():
                 min_score_thresh=.8,
                 agnostic_mode=False)
     imagen = cv2.resize(image_np_with_detections, (800, 600))
+    
+    # Colocando texto en la Imagen
+    font = cv2.FONT_HERSHEY_SIMPLEX
+    org = (350, 500)
+    org2 = (350,550)
+    fontScale = 1.2
+    color = (0, 255, 0)
+    color2 = (0, 0, 255)
+    thickness = 2
+    cv2.putText(imagen, 'Octopy', org, font, 
+                   fontScale, color, thickness, cv2.LINE_AA)
+    cv2.putText(imagen, producto_detec['nombre'], org2, font, 
+                   fontScale, color2, thickness, cv2.LINE_AA)
     cv2.imshow('Deteccion del codigo', imagen)
+
+    # Cálculo del tiempo de ejecución
     t1 = time.time()
     tiempo = t1 - t0
-    #print(f'El tiempo es {tiempo}')
     frames = 1/tiempo
-    t0 = t1
-    #print(f'Frames por segundo: {frames}')
+    t0 = t1        
+    #print(tiempo)
+    #print(frames)
     if cv2.waitKey(10) & 0xFF == ord('q'):
         cap.release()
         cv2.destroyAllWindows()
